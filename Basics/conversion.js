2d69b8a6444e6e6dc1818be311c13af057daa75c
Sohailongithub <sohailshaikh2379@gmail.com>
2024-07-02 00:13:44 +0530

    Conversion of Datatypes

---
+++ Basics/conversion.js
@@ -0,0 +1,11 @@
+let age = 33;
+console.log(`age is ${age}`);
+console.log(typeof age);
+
+let carNum = "Honda2379";
+console.log(`Car No. ${carNum}`);
+console.log(typeof carNum);
+
+let valueInNum = Number(carNum)
+console.log(valueInNum);
+console.log(typeof (carNum));
