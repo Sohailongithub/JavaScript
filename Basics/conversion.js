5b7064c1555dc081e9440dd5de2d5508575e5ef3
Sohailongithub <sohailshaikh2379@gmail.com>
2024-07-04 17:04:31 +0530

    equality and comparison operator check

--- Basics/conversion.js
+++ Basics/conversion.js
@@ -1,11 +1,43 @@
 let age = 33;
-console.log(`age is ${age}`);
-console.log(typeof age);
+// console.log(`age is ${age}`);
+// console.log(typeof age);
 
 let carNum = "Honda2379";
-console.log(`Car No. ${carNum}`);
-console.log(typeof carNum);
+// console.log(`Car No. ${carNum}`);
+// console.log(typeof carNum);
 
 let valueInNum = Number(carNum)
-console.log(valueInNum);
-console.log(typeof (carNum));
+// console.log(valueInNum);
+// console.log(typeof (carNum));
+
+
+// **********************Operations**************************
+
+let str1 = "hello";
+let str2 = " Sohail";
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + '2');
+// console.log(1 + "2" + 2);
+// console.log(3 + 5 * 8 % 9); // operation are performed in default order of precedence: multiplication and modulus before addition.
+// console.log((3 + 5) * 8 % 9); // operations are performed as per parentheses 
+ 
+
+let x = 3;
+const y = x++;
+
+// console.log(`x:${x}, y:${y}`);
+
+console.log(null == 0);
+console.log(null >= 0);
+console.log(null <= 0);
+console.log(null === 0);
+console.log(undefined === 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+console.log(undefined <= 0);
+
+// the equality and comparison operator act here as same that is why the output is true  
