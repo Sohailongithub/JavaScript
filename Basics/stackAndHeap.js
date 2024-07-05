56b01afd66228cf386e4e3a6e2370ef9f110f882
Sohailongithub <sohailshaikh2379@gmail.com>
2024-07-05 19:22:29 +0530

    Stack and Heap Concept

---
+++ Basics/stackAndHeap.js
@@ -0,0 +1,30 @@
+// Stack memory stores primitive datatypes of values and it gives a copy of value 
+
+let fruit = "mango";
+// console.log(fruit);
+
+let fruit1 = "banana";
+// console.log(fruit1);
+
+fruit = "apple" 
+// console.log(fruit);
+
+// Heap memory stores Non-primitive datatypes of values and it gives refrence of value
+
+let user1 = {
+    email: "user1@email.com",
+    Id: "user1Id"
+};
+// console.log(user1);
+
+
+let user2 = {
+    email: "user2@gmail.com",
+    Id: "user2Id"
+};
+// console.log(user2);
+
+
+user2.email = "Gian2@gmail.com"
+// console.log(user2);
+
