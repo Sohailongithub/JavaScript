ffa9b3ffa68c4f9d325e404a106cd1e42923ffe3
Sohailongithub <sohailshaikh2379@gmail.com>
2024-07-05 21:29:55 +0530

    Deeply understand Number and Maths Functions

---
+++ Basics/numAndMath.js
@@ -0,0 +1,9 @@
+let arr = [1,2,3,4,5];
+let sqArr = arr.map(i => i ** 2);
+// console.log(sqArr);
+
+
+let min = 1;
+let max = 6;
+
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min);
