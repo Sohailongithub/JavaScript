dd6954790dc32195f8e899bc2328c5676fe8f999
Sohailongithub <sohailshaikh2379@gmail.com>
2024-07-06 22:46:43 +0530

    understand about objects and arrays

---
+++ Basics/arraysAndObjects.js
@@ -0,0 +1,24 @@
+let sohail = {
+    name: "sohail",
+    age: 23,
+    location: 'mumbai'
+};
+
+let rahul = {
+    name: "rahul",
+    age: 35,
+    location: 'delhi'
+};
+
+let personIntro = (person, age, location) => {
+    return(`Hello this is ${person.name} and my age is ${person.age} I live in ${person.location}`)
+}
+
+console.log(personIntro(rahul));
+console.log(personIntro(sohail));
+
+/////////////////////////////////////////////////
+
+let arr =  [1,2,3,4,5]
+
+console.log(arr.indexOf(5));
