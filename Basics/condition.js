b902ae526e796f3bc9f855fbe0cb980c5bf98ffa
Sohailongithub <sohailshaikh2379@gmail.com>
2024-07-06 21:28:18 +0530

    learn about conditional statements

---
+++ Basics/condition.js
@@ -0,0 +1,17 @@
+let gender = "male";
+let name = "sohail"
+
+gender = 'female';
+name = 'afrin'
+
+gender = 'male';
+name = 'aftab';
+
+if (gender == "female"){
+    console.log(`MRS. ${name}`);
+} else {
+    console.log(`MR. ${name}`);
+}
+
+
+
