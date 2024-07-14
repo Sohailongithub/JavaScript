d9a3a353b1dd4296727fa3bdc3be07d477b94519
Sohailongithub <sohailshaikh2379@gmail.com>
2024-07-14 23:44:43 +0530

    Coffee Machine

--- Basics/condition.js
+++ Basics/condition.js
@@ -1,8 +1,8 @@
-let gender = "male";
-let name = "sohail"
+// let gender = "male";
+// let name = "sohail"
 
-gender = 'female';
-name = 'afrin'
+// gender = 'female';
+// name = 'afrin'
 
 // gender = 'male';
 // name = 'aftab';
@@ -10,14 +10,37 @@ name = 'afrin'
 // gender = "animal"
 // name = "dog"
 
-if (gender == "female"){
-    console.log(`MRS. ${name}`);
-} else if (gender == "male"){
-    console.log(`MR. ${name}`);
-}else {
-    console.log("I dont know you !");
-}
+// if (gender == "female"){
+//     console.log(`MRS. ${name}`);
+// } else if (gender == "male"){
+//     console.log(`MR. ${name}`);
+// }else {
+//     console.log("I dont know you !");
+// }
+
+//////////////////////////////////////////////////////////////////
+//////////////////COFFEE MACHINE//////////////////////////////////
 
 
+let coffeeMachine = (drink) => {
+  switch (drink) {
+    case 1:
+      console.log(`espresso`);
+      break;
+    case 2:
+      console.log(`Cappuccino`);
+      break;
+    case 3:
+      console.log(`Latte`);
+      break;
+    case 4:
+      console.log(`Mocha`);
+    default:
+      console.log("Invaild Selection");
+      break;
+  }
 
+  console.log(`${drink} Your coffee is ready !`);
+};
 
+coffeeMachine(5);
